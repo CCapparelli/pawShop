935452d5729f06c7a13ef1ca218b0599bbe441c4
CCapparelli <kdcapparelli@gmail.com>
2024-10-07 21:16:24 -0300

    Update
    Blog e Checkout

--- src/user/pages/checkout/js/index.js
+++ src/user/pages/checkout/js/index.js
@@ -1,3 +1,5 @@
+var sumOrderTotal = $('#sumOrderTotal');
+
 
 $(window).ready(function() {
     // Your code here
@@ -8,16 +10,23 @@ function refreshCartItems() {
     prodCards.innerHTML = '';
     catalog.items.forEach(item => {
         prodCards.innerHTML += `
-<div class="cartItem w-100 d-flex justify-content-start p-2 bg-azul rounded gap-2">
-    <img class="rounded" src="${catalogImagePath}${item.img}" width="55" height="55">
-    <div class="cardInfo w-100 d-flex flex-column">
-        <p class="m-0 align-self-end">${item.titulo}</p>
-        <div class="w-100 d-flex justify-content-between align-items-center">
-            <h5 class="m-0">${item.preco}</h5>
-            <div class="cArrow m-1">${100}</div>
+<div class="prodCard w-100 d-flex align-items-center justify-content-between">
+    <div class="w-50 px-2 d-flex align-items-center gap-2">
+        <img class="pic" src="${catalogImagePath}${item.img}" alt="">
+        <div class="d-flex flex-column">
+            <span class="fw-bold">${item.titulo}</span> 
+            <div class="d-flex">
+                <span class="pe-3">R$ ${item.preco},00</span>
+                <span class="text-muted text-decoration-line-through">R$ 150,00</span>
+            </div>
         </div>
     </div>
-</div>`;
+    <div class="w-50 d-flex justify-content-start">
+        <span class="pe-3 text-muted">Quantidade: </span>
+        <span class="pe-3">2</span>
+    </div>
+</div>
+`;
     });
-    cartValue.innerText = shoppingCart.total();
+    sumOrderTotal.text('R$ 800,00');
 }
